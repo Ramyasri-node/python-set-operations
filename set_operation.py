b6934b7dfa289e0da7852bb3ff4c12fb0ef9c4d5
Ramyasri <ramyasrisivasankar@gmail.com>
2026-02-12 09:12:37 +0530

    Add files via upload

---
+++ set_operation.py
@@ -0,0 +1,17 @@
+# Program to illustrate different set operations
+
+# Define two sets
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5, 6, 8}
+
+# Union
+print("Union of E and N is", E | N)
+
+# Intersection
+print("Intersection of E and N is", E & N)
+
+# Difference
+print("Difference of E and N is", E - N)
+
+# Symmetric Difference
+print("Symmetric difference of E and N is", E ^ N)
